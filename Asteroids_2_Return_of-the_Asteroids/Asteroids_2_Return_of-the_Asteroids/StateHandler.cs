a458684c9a5b8b2b3eba76e9db2faf40d658f16a
HAxan Simon <simon.ohr@live.se>
2018-02-07 15:22:52 +0100

    Update
    Seems it was a matter of VS 2015 not being able to handle some features
    in VS 2017, booting from 2015 resulted in many an error, booting from
    2017 resulted in no issues. Good to know.

---
+++ Asteroids_2_Return_of-the_Asteroids/Asteroids_2_Return_of-the_Asteroids/StateHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids_2_Return_of_the_Asteroids
+{
+    class StateHandler
+    {
+    }
+}
